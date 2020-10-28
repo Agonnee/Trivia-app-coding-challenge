b44006d6db5d8ba072988e560ba0d10a4d72ed0c
Agonnee <67187469+Agonnee@users.noreply.github.com>
2020-10-27 23:57:02 -0700

    Added quiz generator and questions class.
    Questions class is still not fully functional, full 10 questions don't seem to be created.

--- Trivia-app.py
+++ Trivia-app.py
@@ -4,24 +4,56 @@ import random
 import os
 
 #Defining classes
+class Singleton:
 
+    def __init__(self, prompt, wrongs, right):
+        self.prompt = prompt
+        self.wrongs = wrongs
+        self.right = right
+
+    @property
+    def answers():
+        self.wrongs.append(self.right)
+        self.wrongs.sort()
+
+class TheQuiz:
+    def __init__(self, singles=None):
+        self.singles = singles
+
+#Generating questions for the quiz
+    @classmethod
+    def create_thequiz(cls, dataset):
+        singles = []
+        for single in dataset[0:10]:
+            for key in single.keys():
+                singles.append(Singleton(single['question'], single['incorrect'], single['correct']))
+            return cls(singles)
 
 #change directory to that of the script
-abspath = os.path.abspath(__file__)
-dname = os.path.dirname(abspath)
-os.chdir(dname)
+os.chdir(os.path.dirname(__file__))
+
 
 #Parsing the JSON file for the question generator
 with open('Apprentice_TandemFor400_Data.json') as f:
-  data = json.load(f)
+  dataset = json.load(f)
 
-print(data)
-input("what now")
+random.shuffle(dataset)
+
+##print(data[0])
+
+##for qstn in dataset[0:10]:
+##    for v in qstn.values():
+##        print(v)
+
+
+
+
+##print(qz)
+##print(qz.singles)
+print(str(qz.singles[0]))
 
 #Welcome to the Trivia Quiz
-
-
-#Generating questions for the quiz
+qz = TheQuiz.create_thequiz(dataset)
 
 
 #asking the user the questions 1 at a time
@@ -34,3 +66,6 @@ input("what now")
 
 
 #Resulting Score, play again prompt
+
+
+input("what now")  #Holding console open for informational purposes
